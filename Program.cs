e811bf9ba9048f3e129eb32c5ee0d5b6e4b00780
Abdullateef Adebayo <lateefaladifa1@gmail.com>
2022-04-19 15:20:16 +0100

    lab 1 committ

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello! My Name is Abdullateeef Adebayo, and I am in Backend Class of Wella Schools");
+            Console.WriteLine("I am learning Backend to become a good programer of what is happening behind the scene in website");
+        }
+    }
+}
